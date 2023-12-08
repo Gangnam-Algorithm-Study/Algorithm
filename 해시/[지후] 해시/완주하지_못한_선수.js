3e2f3db200577fe3a63a498a737cea32d65e9d1b
jihooDev <wlgn829@kakao.com>
2023-12-08 09:55:25 +0900

    feat: [지후] 해시

--- 해시/[지후] 해시/완주하지_못한_선수.js
+++ 해시/[지후] 해시/완주하지_못한_선수.js
@@ -1,12 +1,36 @@
-function solution(participant, completion) {
-    // 시간 복잡도 문제
-    // for문으로 변경
-    const sortParticipant = participant.sort();
-    const sortCompletion = completion.sort();
+// function solution(participant, completion) {
+//     // 시간 복잡도 문제
+//     // for문으로 변경
+//     const sortParticipant = participant.sort();
+//     const sortCompletion = completion.sort();
 
-    for (let i = 0; i <= sortParticipant.length; i++) {
-        if (sortParticipant[i] !== sortCompletion[i]) {
-            return sortParticipant[i];
+//     for (let i = 0; i <= sortParticipant.length; i++) {
+//         if (sortParticipant[i] !== sortCompletion[i]) {
+//             return sortParticipant[i];
+//         }
+//     }
+// }
+
+
+// 두번째 방식
+// Map을 사용한 방식으로 변경
+function solution(participant, completion) {
+    const participantMap = new Map();
+
+    participant.forEach((value) => {
+        if (participantMap.has(value)) {
+            participantMap.set(`${value}`, participantMap.get(`${value}`) + 1);
+        } else {
+            participantMap.set(value, 1);
         }
-    }
+    });
+
+    completion.forEach((value) => {
+        participantMap.set(`${value}`, participantMap.get(`${value}`) - 1);
+        if (participantMap.get(`${value}`) === 0) {
+            participantMap.delete(`${value}`);
+        }
+    });
+
+    return Array.from(participantMap)[0][0];
 }
--- 해시/[지후] 해시/폰켓몬.js
+++ 해시/[지후] 해시/폰켓몬.js
@@ -1,6 +1,24 @@
+// 첫번째 방식
+// 중복값들을 제거하여 해당하는 갯수를 반환
 function solution(nums) {
     const max = nums.length / 2;
     const result = [...new Set(nums)];
 
     return result.length > max ? max : result.length;
+}
+
+// 두번째 방식
+// 중복값을 제거하는 알고리즘을 Set에서 -> 배열을 사용하는 방법으로 변경
+
+function solution(nums) {
+    const max = nums.length / 2;
+    let parseNums = [];
+
+    for (let i = 0; i < nums.length; i++) {
+        if (!parseNums.includes(nums[i])) {
+            parseNums.push(nums[i]);
+        }
+    }
+
+    return parseNums.length > max ? max : parseNums.length;
 }
