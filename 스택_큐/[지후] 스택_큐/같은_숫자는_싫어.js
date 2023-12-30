15ba6a3313cbbbc338eee07d4d58c164ac0995b1
jihooDev <wlgn829@kakao.com>
2023-12-30 09:57:36 +0900

    feat: 스택 / 큐 제출

--- 스택_큐/[지후] 스택_큐/같은_숫자는_싫어.js
+++ 스택_큐/[지후] 스택_큐/같은_숫자는_싫어.js
@@ -1,3 +1,20 @@
+// function solution(arr) {
+//     return arr.filter((item, idx) => item !== arr[idx + 1]);
+// }
+
+// 스택을 사용한 풀이법
 function solution(arr) {
-    return arr.filter((item, idx) => item !== arr[idx + 1]);
+    let stack = [];
+
+    arr.forEach(nums => {
+        if (stack.length === 0) {
+            stack.push(nums);
+        }
+
+        if (stack[stack.length - 1] !== nums) {
+            stack.push(nums);
+        }
+    })
+
+    return stack;
 }
